58d20c2ce8c980be99ccc1826137d31097b51f20
pipe473 <bedoyafelipe743@gmail.com>
2020-12-03 13:30:55 +0100

    creando rutas para api, utilizando el modulo de ROUTER de express

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Configuracion de base de datos
 require('./db');
 
+//Importando las rutas
+app.use(require('./routes/index'));
 
+
+// Puerto Servidor
 app.listen(3000);
 console.log('Server on Port', 3000);
 
