709f9be3d2c1cf3071068c0a804685b7d0ba8787
pipe473 <bedoyafelipe743@gmail.com>
2020-12-03 19:05:45 +0100

    creando los componentes que voy a utilizar en la aplicacion

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    TasksComponent,
+    PrivateTasksComponent
   ],
   imports: [
     BrowserModule,
