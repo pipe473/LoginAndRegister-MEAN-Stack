a6bdd0bef52bc626cff89205e25e32b4ebd8abd5
pipe473 <bedoyafelipe743@gmail.com>
2020-12-03 14:50:35 +0100

    añadiendo token utilizando libreria de jsonwebtoken

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,13 +3,18 @@ const router = Router();
 
 const User = require('../models/User');
 
+const jwt = require('jsonwebtoken');
+
 router.get('/', (req, res) => res.send('Testing...'))
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
     const newUser = new User({ email, password });
     await newUser.save();   
-    res.send('Testing SignUp');
+   
+    const token = jwt.sign({ _id: newUser._id }, 'secretKey' )
+    res.status(200).json({token})
+
 })
 
 module.exports = router;
