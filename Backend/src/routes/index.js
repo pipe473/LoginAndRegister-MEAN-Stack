a258649df096d3e9034319c1bc8d1fee35412991
pipe473 <bedoyafelipe743@gmail.com>
2020-12-03 13:36:46 +0100

    creando metodo post para registrar usuarios

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
+const User = require('../models/User');
+
 router.get('/', (req, res) => res.send('Testing...'))
+
+router.post('/signup', (req, res) => {
+    res.send('Register');
+})
 
 module.exports = router;
