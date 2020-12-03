e59c00e0fcd19190958c53adfaf66b481d52dfb9
pipe473 <bedoyafelipe743@gmail.com>
2020-12-03 20:07:10 +0100

    recogiendo los datos del formulario con ngModule

--- Frontend/src/app/components/signup/signup.component.ts
+++ Frontend/src/app/components/signup/signup.component.ts
@@ -7,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  user = {
+    email: '',
+    password: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   signUp(){
-    
+
   }
 
 }
