00d217edd0d317cddd7be2d3088509c834775ef7
pipe473 <bedoyafelipe743@gmail.com>
2020-12-04 16:58:36 +0100

    solucionando el problema de cors desde el backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Configuracion de base de datos
 require('./db');
+
+
+// Importando el modulo de cors para poder conectar 
+// las cabeceras del backend con el frontend
+app.use(cors());
 
 // Metodo de express que transforma los datos 
 // que recibe del servidor en formato JSON
