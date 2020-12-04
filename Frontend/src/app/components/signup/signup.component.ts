7c9b5e7cc5bb743046943bd342001e3d619d80e0
pipe473 <bedoyafelipe743@gmail.com>
2020-12-04 16:53:56 +0100

    añadiendo al app.module httpClientModule para resolver el error de comunicacion del componente

--- Frontend/src/app/components/signup/signup.component.ts
+++ Frontend/src/app/components/signup/signup.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
   user = {
     email: '',
-    password: ''
+    password: '',
+  };
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  signUp() {
+    this.authService.register(this.user).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => console.log(err)
+    );
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  signUp(){
-    console.log(this.user);    
-  }
-
 }
