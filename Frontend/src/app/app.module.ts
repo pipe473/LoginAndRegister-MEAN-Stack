ebbf7ce9bf66bb093ee90c6e5fc04ffdcf2f9b84
pipe473 <bedoyafelipe743@gmail.com>
2020-12-04 17:49:35 +0100

    añadiendo el guard para poder usarlo en cualquier lugar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from './auth.guard' 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,9 @@ import { PrivateTasksComponent } from './components/private-tasks/private-tasks.
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
