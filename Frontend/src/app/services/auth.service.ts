11c1b83d6f08cc02e477dcb81d55c9e45b278b9d
pipe473 <bedoyafelipe743@gmail.com>
2020-12-04 16:43:05 +0100

    creando la comunicacion entre el servicio y el backend

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/api'
+
+  constructor(private http: HttpClient) { }
+
+  register(user){
+    return this.http.post<any>(this.url + '/signup', user);
+  }
 }
